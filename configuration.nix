f29a5398698f3481c362835221e904db7165ce7b
Ryan Walker <ryanjwalker2001@gmail.com>
2023-05-02 07:10:13 +0100

    gnome

--- configuration.nix
+++ configuration.nix
@@ -16,12 +16,23 @@ in
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./gnome.nix
+      # ./gnome.nix
       # ./kde.nix
       # <home-manager/nixos>
       # ./ryan.nix
       # /home/ryan/config.nix
     ];
+    
+    
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  
+  
+  environment.systemPackages = with pkgs; [
+  	gnome-extension-manager
+    gnome.gnome-tweaks
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;

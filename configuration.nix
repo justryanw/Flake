728bdc78ce7fdc0926044a8be83abef54199bccd
Ryan Walker <ryanjwalker2001@gmail.com>
2023-05-02 14:50:54 +0100

    vm default

--- configuration.nix
+++ configuration.nix
@@ -16,12 +16,6 @@
   #   gnome.gnome-tweaks
   # ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-
-  networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -1,7 +1,13 @@
-{ config, pkgs, lib, ... }:
-{
+{ config, pkgs, ... }: {
   imports = [
-      ./hardware-configuration.nix
+    ./hardware-configuration.nix
   ];
 
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
+  }
+
+  networking.hostName = "NixVM";
 }

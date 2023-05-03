20a521ce1f4ebdb0f44b3ab961840b2591e34e42
Ryan <ryanjwalker2001@gmail.com>
2023-05-03 14:58:47 +0100

    grub setup

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,16 +1,31 @@
-{ config, pkgs, ... }: {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+{ pkgs, ... }: {
+  imports = [ ./hardware-configuration.nix ];
 
   boot = {
     loader = {
-      systemd-boot.enable = true;
+      systemd-boot.enable = false;
       efi = {
         canTouchEfiVariables = true;
         efiSysMountPoint = "/boot/efi";
       };
+
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiSupport = true;
+        useOSProber = true;
+        fontSize = 32;
+        gfxmodeEfi = "3440x1440";
+        extraEntries = ''
+          menuentry 'System setup' $menuentry_id_option 'uefi-firmware' {
+            fwsetup
+          }
+        '';
+      };
+
     };
+
+
     supportedFilesystems = [ "ntfs" ];
     kernelPackages = pkgs.linuxPackages_latest;
   };

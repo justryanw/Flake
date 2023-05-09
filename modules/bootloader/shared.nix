3df7e9e4697669a891d574c4fcc064afbc7d47d7
Ryan <ryanjwalker2001@gmail.com>
2023-05-09 15:08:39 +0100

    remove conficting

--- modules/bootloader/shared.nix
+++ modules/bootloader/shared.nix
@@ -2,15 +2,9 @@
   boot = {
     loader = {
       systemd-boot.enable = false;
-      efi = {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot/efi";
-      };
 
       grub = {
         enable = true;
-        device = "nodev";
-        efiSupport = true;
         useOSProber = true;
         font = "${pkgs.hack-font}/share/fonts/hack/Hack-Regular.ttf";
         fontSize = 24;

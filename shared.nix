f47e7696604cb618f39b80de652f91c973c9536c
Ryan <ryanjwalker2001@gmail.com>
2023-05-16 11:04:51 +0100

    garbage

--- shared.nix
+++ shared.nix
@@ -158,5 +158,16 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+    };
+  };
 }

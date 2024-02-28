7906abc013af56d7c512b9215fc7c7e0d2e1523e
Ryan Walker <ryanjwalker2001@gmail.com>
2024-02-28 22:04:29 +0000

    prime

--- hosts/pavilion/default.nix
+++ hosts/pavilion/default.nix
@@ -1,22 +1,32 @@
-{ config, ... }: {
+{ config, lib, ... }: {
   imports = [ ./hardware-configuration.nix ];
 
-  boot.loader.grub.gfxmodeEfi = "1920x1080";
+  boot = {
+    loader.grub.gfxmodeEfi = "1920x1080";
+  };
 
   networking.hostName = "Pavilion";
 
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = lib.mkForce [ "nvidia" ];
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    
-    prime = {
-      sync.enable = true;
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
 
-      amdgpuBusId = "PCI:5:0:0";
-      nvidiaBusId = "PCI:1:0:0";
+    nvidia = {
+      modesetting.enable = true;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+        prime = {
+          sync.enable = true;
+
+          amdgpuBusId = "PCI:5:0:0";
+          nvidiaBusId = "PCI:1:0:0";
+        };
     };
   };
 }

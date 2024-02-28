1156152416117c9901128ff5536cf59234eff575
Ryan Walker <ryanjwalker2001@gmail.com>
2024-02-28 21:03:26 +0000

    nvidia

--- hosts/pavilion/default.nix
+++ hosts/pavilion/default.nix
@@ -1,7 +1,15 @@
-{ ... }: {
+{ config, ... }: {
   imports = [ ./hardware-configuration.nix ];
 
   boot.loader.grub.gfxmodeEfi = "1920x1080";
 
   networking.hostName = "Pavilion";
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }

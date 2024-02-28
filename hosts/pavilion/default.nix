d96a57d2cfad590f7fed586fb138e286f6d53adf
Ryan Walker <ryanjwalker2001@gmail.com>
2024-02-28 21:19:38 +0000

    prime

--- hosts/pavilion/default.nix
+++ hosts/pavilion/default.nix
@@ -11,5 +11,12 @@
     modesetting.enable = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+    
+    prime = {
+      sync.enable = true;
+
+      amdgpuBusId = "PCI:5:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 }

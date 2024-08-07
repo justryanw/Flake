b648ba77efdb94b264ff8154fba6a8387debbc86
Ryan Walker <ryanjwalker2001@gmail.com>
2024-08-07 16:22:34 +0100

    work module

--- modules/home/work.nix
+++ modules/home/work.nix
@@ -1,11 +1,21 @@
-{ pkgs, lib, config, ... }: {
+{ pkgs, lib, config, rootConfig, ... }: {
   options = {
     work.enable = lib.mkEnableOption "Enable work software";
   };
 
-  config = lib.mkIf config.work.enable {
-    home.packages = with pkgs; [
-      teams-for-linux
-    ];
-  };
+  config = lib.mkMerge [
+    (lib.mkIf config.work.enable {
+      home.packages = with pkgs; [
+        teams-for-linux
+        remmina
+      ];
+    })
+    (lib.mkIf (config.work.enable && rootConfig.modules.gnome.enable) {
+      home.packages = with pkgs.gnomeExtensions; [ remmina-search-provider ];
+
+      dconf.settings."org/gnome/shell".enabled-extensions = [ "remmina-search-provider@alexmurray.github.com" ];
+
+      programs.vscode.userSettings.remote.SSH.remotePlatform.inspired = "windows";
+    })
+  ];
 }

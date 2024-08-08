c7ea71f0256953fcb36baa142a0da876df20fcef
Ryan Walker <ryanjwalker2001@gmail.com>
2024-08-08 10:19:07 +0100

    work ssh

--- modules/home/work.nix
+++ modules/home/work.nix
@@ -5,13 +5,37 @@
 
   config = lib.mkMerge [
     (lib.mkIf config.modules.work.enable {
-      home.packages = with pkgs; [
-        teams-for-linux
-        remmina
-      ];
+      programs.ssh.matchBlocks = {
+        inspired = {
+          user = "ryan.walker";
+        };
+
+        wsl = {
+          hostname = "localhost";
+          proxyJump = "inspired";
+          port = 2224;
+        };
+
+        marketing = {
+          user = "dev";
+          hostname = "gamepreview.inseinc.com";
+          identityFile = [ "/home/ryan/.ssh/marketing/id_rsa" ];
+        };
+
+        gli = {
+          user = "gds";
+          hostname = "ftp.ingg.com";
+        };
+      };
     })
     (lib.mkIf (config.modules.work.enable && rootConfig.modules.gnome.enable) {
-      home.packages = with pkgs.gnomeExtensions; [ remmina-search-provider ];
+
+      home.packages = (with pkgs; [
+        teams-for-linux
+        remmina
+      ]) ++ (with pkgs.gnomeExtensions; [
+        remmina-search-provider
+      ]);
 
       dconf.settings."org/gnome/shell".enabled-extensions = [ "remmina-search-provider@alexmurray.github.com" ];
 

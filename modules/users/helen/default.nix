d4b6fca2062fd31a5f72aa19ff97a044370e096c
Ryan Walker <ryanjwalker2001@gmail.com>
2024-08-10 03:08:51 +0100

    hashed pass

--- modules/users/helen/default.nix
+++ modules/users/helen/default.nix
@@ -1,5 +1,7 @@
 name: { pkgs, lib, config, ... } @ inputs: {
   config = lib.mkIf config.modules.users.${name}.enable {
-    users.users.${name} = { };
+    users.users.${name} = {
+      initialHashedPassword = "$y$j9T$VcFtf.LY2BA.ICsKVYa.X.$lEGovq2oX/EwnV5Q/8Rj2vehlUi3fWaP3UIayolWSU8";
+    };
   };
 }

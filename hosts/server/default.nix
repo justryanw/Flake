23088937bb6cb730ef5e5067b6217fefff653264
Ryan Walker <ryanjwalker2001@gmail.com>
2024-08-11 04:39:18 +0100

    mullvad blocking workaround

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -21,6 +21,26 @@
         Settings.AutoConnect = true;
       };
     };
+
+    # Exclude yggdrasil from being blocked by mullvad
+    nftables = {
+      enable = true;
+      tables.excludeTraffic = {
+        name = "excludeTraffic";
+        family = "inet";
+        content = ''
+          chain allowIncomming {
+            type filter hook input priority -100; policy accept;
+            ip6 daddr 200::/7 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+          }
+
+          chain excludeOutgoing {
+            type route hook output priority -100; policy accept;
+            ip6 daddr 200::/7 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+          }
+        '';
+      };
+    };
   };
 
   services.mullvad-vpn = {

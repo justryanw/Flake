c8e21ccdeb11a24138721f0b1bf73a69a43f3d56
Ryan Walker <ryanjwalker2001@gmail.com>
2024-08-11 02:09:33 +0100

    server config

---
+++ hosts/server/default.nix
@@ -0,0 +1,15 @@
+{ ... }: {
+  imports = [
+    ../../configuration.nix
+    ./hardware-configuration.nix
+  ];
+
+  modules = {
+    gaming.enable = false;
+    users.helen.enable = false;
+    amd.enable = false;
+  };
+
+  networking.hostName = "server";
+  system.stateVersion = "24.11";
+}

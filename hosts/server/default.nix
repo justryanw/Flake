b517e1a8928aab3bc0c64e96669a1d70706ba7a2
Ryan Walker <ryanjwalker2001@gmail.com>
2024-08-11 05:19:09 +0100

    test2

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -41,12 +41,12 @@
         content = ''
           chain allowIncomming {
             type filter hook input priority -100; policy accept;
-            ip6 daddr 200::/7 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+            ip6 saddr 202:9cf8:d9b1:83e5:f832:c74e:8fb7:e6c9 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
           }
 
           chain excludeOutgoing {
             type route hook output priority -100; policy accept;
-            ip6 daddr 202:9cf8:d9b1:83e5:f832:c74e:8fb7:e6c9 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+            ip6 daddr 200::/7 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
           }
         '';
       };

a980d542deb7dec235f75192730ab14999a941e9
Ryan Walker <ryanjwalker2001@gmail.com>
2024-11-10 01:30:22 +0000

    disable mangohud session wide

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -3,7 +3,7 @@
     programs = {
       mangohud = {
         enable = true;
-        enableSessionWide = true;
+        enableSessionWide = false;
 
         settings = {
           fps_limit = 120;

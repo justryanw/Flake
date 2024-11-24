5d16b2d82887ba954d12f23c54feae07242a4715
Ryan Walker <ryanjwalker2001@gmail.com>
2024-11-24 06:25:20 +0000

    remove other builders

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -11,45 +11,4 @@
 
   networking.hostName = "laptop";
   system.stateVersion = "22.11";
-
-  nix = {
-    buildMachines = [
-      {
-        hostName = "server";
-        system = "x86_64-linux";
-        protocol = "ssh-ng";
-        sshUser = "ryan";
-        sshKey = "/home/ryan/.ssh/id_ed25519";
-        maxJobs = 6;
-        speedFactor = 2;
-        supportedFeatures = [
-          "nixos-test"
-          "benchmark"
-          "big-parallel"
-          "kvm"
-        ];
-        mandatoryFeatures = [ ];
-      }
-      {
-        hostName = "desktop";
-        system = "x86_64-linux";
-        protocol = "ssh-ng";
-        sshUser = "ryan";
-        sshKey = "/home/ryan/.ssh/id_ed25519";
-        maxJobs = 8;
-        speedFactor = 2;
-        supportedFeatures = [
-          "nixos-test"
-          "benchmark"
-          "big-parallel"
-          "kvm"
-        ];
-        mandatoryFeatures = [ ];
-      }
-    ];
-    distributedBuilds = true;
-    settings = {
-      builders-use-substitutes = true;
-    };
-  };
 }

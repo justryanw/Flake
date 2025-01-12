2f7418d248fb46d37d7234057c9eb587cb8ab44a
Ryan Walker <ryanjwalker2001@gmail.com>
2025-01-12 23:21:25 +0000

    fix state v

--- hosts/jupiter/default.nix
+++ hosts/jupiter/default.nix
@@ -13,5 +13,5 @@
   disko.devices.disk.${config.networking.hostName}.device = "/dev/disk/by-id/nvme-eui.6479a759203003dd";
 
   networking.hostName = "jupiter";
-  system.stateVersion = "25.01";
+  system.stateVersion = "25.05";
 }

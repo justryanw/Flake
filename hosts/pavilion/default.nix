6867ec65dab8fdee74d9efe91f1dc980f657b8d9
Ryan Walker <ryanjwalker2001@gmail.com>
2025-01-22 15:32:01 +0000

    pavilion

--- hosts/pavilion/default.nix
+++ hosts/pavilion/default.nix
@@ -15,10 +15,35 @@
     fsType = "ext4";
   };
 
-  disko.devices.disk.${config.networking.hostName}.device = "/dev/disk/by-id/nvme-eui.e8238fa6bf530001001b444a44d43292";
+  disko.devices.disk.${config.networking.hostName}.device =
+    "/dev/disk/by-id/nvme-eui.e8238fa6bf530001001b444a44d43292";
 
   boot.loader.grub.gfxmodeEfi = "1920x1080";
 
   networking.hostName = "pavilion";
   system.stateVersion = "24.11";
+
+  services.xmrig = {
+    enable = true;
+    settings = {
+      autosave = true;
+      cpu = {
+        enabled = true;
+        rx = {
+          threads = 6;
+        };
+      };
+      opencl = false;
+      cuda = false;
+      pools = [
+        {
+          url = "pool.hashvault.pro:443";
+          user = "84jLA5hxrGkNrj7kLpZt519MCWwPyMj8oBt9ikTAqoZvG8Qcd3PFGmkZNDPDT9jk7FZ39VzNMgqzFXLHEKvs9pcF6L8DaTm";
+          pass = "server";
+          keepalive = true;
+          tls = true;
+        }
+      ];
+    };
+  };
 }

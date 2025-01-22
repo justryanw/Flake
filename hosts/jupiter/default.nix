c21eede2adad4a8053955f4025ce317ccd7f435c
Ryan Walker <ryanjwalker2001@gmail.com>
2025-01-22 15:39:30 +0000

    jupiter

--- hosts/jupiter/default.nix
+++ hosts/jupiter/default.nix
@@ -11,8 +11,33 @@
     users.helen.enable = false;
   };
 
-  disko.devices.disk.${config.networking.hostName}.device = "/dev/disk/by-id/nvme-eui.6479a759203003dd";
+  disko.devices.disk.${config.networking.hostName}.device =
+    "/dev/disk/by-id/nvme-eui.6479a759203003dd";
 
   networking.hostName = "jupiter";
   system.stateVersion = "25.05";
+
+  services.xmrig = {
+    enable = true;
+    settings = {
+      autosave = true;
+      cpu = {
+        enabled = true;
+        rx = {
+          threads = 6;
+        };
+      };
+      opencl = false;
+      cuda = false;
+      pools = [
+        {
+          url = "pool.hashvault.pro:443";
+          user = "84jLA5hxrGkNrj7kLpZt519MCWwPyMj8oBt9ikTAqoZvG8Qcd3PFGmkZNDPDT9jk7FZ39VzNMgqzFXLHEKvs9pcF6L8DaTm";
+          pass = "server";
+          keepalive = true;
+          tls = true;
+        }
+      ];
+    };
+  };
 }
